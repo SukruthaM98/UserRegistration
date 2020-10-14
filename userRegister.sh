34d8629c43600744b6eb976329431007df060692
SukruthaM98 <msukrutha@gmail.com>
2020-10-14 09:12:59 +0530

    user first name validated

--- userRegister.sh
+++ userRegister.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "welcome to user registration program"
+read -p "enter valid first name containing first letter as capital and length of minimum 3 letters:" username
+
+usernamepattern="^[[:upper:]][[:lower:]]{3,}"
+
+if [[ $username =~ $usernamepattern ]]
+then
+	echo " valid username"
+else
+	echo "invalid username"
+fi
